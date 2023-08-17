2252c7b98401220ed3f8c873a5e21c79cce883c9
Gowtham <106096598+g-wtham@users.noreply.github.com>
2023-08-17 20:15:45 +0530

    Added API Functionality, better user interaction
    - API Functionality
    - Wrapped the entire thing inside a function to enhance reusability
    - Added 'Try Again' button at the endscreen
    - Added the condition to remove the startscreen info, during the typing part, based on condition, as it was interfering when I enclosed the main website logic into a separate function called startGame(), when it is called again further, which throwed up error. So, moved the start screen para before the function starts, and made it as a global variable.

--- typing_game.js
+++ typing_game.js
@@ -1,17 +1,5 @@
-//array of quotes//
-
-const quotes = [
-    'When you have eliminated the impossible, whatever remains, however improbable, must be the truth.',
-    'There is nothing more deceptive than an obvious fact.',
-    'I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.',
-    'I never make exceptions. An exception disproves the rule.',
-    'What one man can invent another can discover.',
-    'Nothing clears up a case so much as stating it to another person.',
-    'Education never ends, Watson. It is a series of lessons, with the greatest for the last.',
-    ]
-
 //empty array variable to store a quote//
-
+quote=[];
 let words = [];
 let wordIndex = 0;
 
@@ -21,68 +9,86 @@ const quoteEle = document.getElementById('quote');
 const messageEle = document.getElementById('message');
 const typedEle = document.getElementById('typed-value');
 const startBtn = document.getElementById('start');
+const infoPara = document.getElementById('info-para')
 
-startBtn.addEventListener('click', ()=>{
+function startGame() {
+    apiURL = 'https://type.fit/api/quotes';
 
-    const infoPara = document.getElementById('info-para')
-    infoPara.remove();
-    
-    const quotesIndex = Math.floor(Math.random() * quotes.length);
-    const quote = quotes[quotesIndex];
+    fetch(apiURL).then(response=>response.json()).then(data=>{
 
-    const words = quote.split(' ');
+        const quotesIndex = Math.floor(Math.random() * data.length);
+        const quote = data[quotesIndex].text;
 
-    for(const word of words){
-        const spanWords = document.createElement('span');
-        spanWords.textContent = word + ' ';
-        quoteEle.appendChild(spanWords);
+        if (infoPara) {
+            infoPara.remove();
+        }
+
+        words = quote.split(' ');
+
+        for (const word of words) {
+            const spanWords = document.createElement('span');
+            spanWords.textContent = word + ' ';
+            quoteEle.appendChild(spanWords);
+        }
+
+        quoteEle.childNodes[0].className = 'highlight-currentWord';
+
+        messageEle.innerHTML = '';
+
+        typedEle.innerHTML = '';
+        //setting the typing value to empty and focusing//
+
+        typedEle.focus();
+
+        startTime = new Date().getTime();
+
     }
-
-    quoteEle.childNodes[0].className = 'highlight-currentWord';
-
-    messageEle.innerHTML = '';
-
-    typedEle.innerHTML = ''; //setting the typing value to empty and focusing//
-
-    typedEle.focus();
-
-    startTime = new Date().getTime();
-
+    )
     //typing tracking part//
 
-    typedEle.addEventListener('input', ()=> {
+    typedEle.addEventListener('input', ()=>{
         const currentWord = words[wordIndex];
         const typedValue = typedEle.value;
 
-        if (typedValue === currentWord && wordIndex === words.length-1){
+        if (typedValue === currentWord && wordIndex === words.length - 1) {
             const elapsedTime = new Date().getTime() - startTime;
-            const message = `Congrats! You finished in ${elapsedTime/1000} seconds!`;
+            const message = `Congrats! You finished in ${elapsedTime / 1000} seconds!`;
             messageEle.innerText = message;
             quoteEle.remove();
+            typedEle.blur();
             typedEle.remove();
 
-            const endImg = document.getElementById('endImg')
+            const endImg = document.getElementById('endImg');
             endImg.style.display = 'block';
-        }
 
-        else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord){
-            
+            const newLine = document.createElement('br');
+            messageEle.appendChild(newLine);
+
+            const tryAgainBtn = document.createElement('button');
+            tryAgainBtn.textContent = 'Try Again!?';
+            messageEle.appendChild(tryAgainBtn);
+            tryAgainBtn.addEventListener('click', ()=>{
+                location.reload();
+            }
+            );
+
+        } else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
+
             wordIndex++;
             typedEle.value = '';
 
-            for(const wordEle of quoteEle.childNodes){
-                wordEle.className = ''; 
+            for (const wordEle of quoteEle.childNodes) {
+                wordEle.className = '';
             }
 
             quoteEle.childNodes[wordIndex].className = 'highlight-currentWord';
-        }
-
-        else if (currentWord.startsWith(typedValue)){
+        } else if (currentWord.startsWith(typedValue)) {
             typedEle.className = '';
-        } 
-
-        else {
+        } else {
             typedEle.className = 'error';
         }
-    });
-});
+    }
+    );
+}
+
+startBtn.addEventListener('click', startGame)
